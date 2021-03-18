114eae6f94691e31c1af8c8763eaca10eeaaee5d
Steve Brownlee <chortlehoort@gmail.com>
2021-03-18 10:22:47 -0500

    Using custom event

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,35 +1,34 @@
 import { getMetals, getOrders, getSizes, getStyles } from "../data/database.js"
 
-
-const buildOrderListItem = (order) => {
-    const metals = getMetals()
-    const sizes = getSizes()
-    const styles = getStyles()
-
-    const metalPrice = metals.find(m => m.id === order.metalId).price
-    const sizePrice = sizes.find(s => s.id === order.sizeId).price
-    const stylePrice = styles.find(st => st.id === order.styleId).price
-
-    let totalCost = metalPrice + stylePrice + sizePrice
-
-    if (order.type === "earrings") {
-        totalCost *= 2
-    }
-
-    if (order.type === "necklace") {
-        totalCost *= 4
-    }
-
-    return <li key={order.id}>
-        Order #{order.id} cost {totalCost.toLocaleString("en-US", {
-            style: "currency",
-            currency: "USD"
-        })}
-    </li>
-}
-
 export const Orders = () => {
     const orders = getOrders()
+
+    const buildOrderListItem = (order) => {
+        const metals = getMetals()
+        const sizes = getSizes()
+        const styles = getStyles()
+
+        const metalPrice = metals.find(m => m.id === order.metalId).price
+        const sizePrice = sizes.find(s => s.id === order.sizeId).price
+        const stylePrice = styles.find(st => st.id === order.styleId).price
+
+        let totalCost = metalPrice + stylePrice + sizePrice
+
+        if (order.type === "earrings") {
+            totalCost *= 2
+        }
+
+        if (order.type === "necklace") {
+            totalCost *= 4
+        }
+
+        return <li key={order.id}>
+            Order #{order.id} cost {totalCost.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD"
+            })}
+        </li>
+    }
 
     return (
         <ul>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const renderApp = () => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+}
+
+renderApp()
+
+document.addEventListener("stateChanged", event => {
+    console.log("State of data has changed. Regenerating HTML...")
+    renderApp()
+})
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
